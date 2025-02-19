5486006397c0928ec260ea5decd44a0993a03bee
1ris_W <ediath462@gmail.com>
2025-02-19 19:42:59 +0800

    Initial Commit

---
+++ BuphagusAfricanus-Demo/BuphagusAfricanus_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuphagusAfricanus_DemoApp.swift
+//  BuphagusAfricanus-Demo
+//
+//  Created by Iris on 2025-02-19.
+//
+
+import SwiftUI
+
+@main
+struct BuphagusAfricanus_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuphagusAfricanus-Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BuphagusAfricanus-Demo
+//
+//  Created by Iris on 2025-02-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
